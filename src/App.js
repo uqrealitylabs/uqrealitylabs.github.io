ca4239e83ec4693350c657734f400a302d10a0f6
tranjamin <55745642+tranjamin@users.noreply.github.com>
2023-12-08 09:39:34 +1000

    sets up firebase init and app integration

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { initializeApp } from "firebase/app";
+import { } from "firebase/analytics";
+import { } from "firebase/firestore";
+import { } from "firebase/functions";
+
+const firebaseConfig = {
+
+};
+
+const fbApp = initializeApp(firebaseConfig);
+
 function App() {
   return (
     <div className="App">
