ed12a65347b84c85910dc05029d41d3c59be4b86
tranjamin <55745642+tranjamin@users.noreply.github.com>
2023-12-16 21:01:12 +1000

    more navbar styling

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import { } from "firebase/functions";
 
 import Navbar from 'react-bootstrap/Navbar';
 import { Nav } from 'react-bootstrap';
-import Container from 'react-bootstrap/Container';
 
 const firebaseConfig = {
 
@@ -35,45 +34,23 @@ function App() {
 
 function Navigation() {
   return (
-      <Navbar expand="lg" className="bg-body-tertiary main-nav d-flex align-items" variant="underline">
+      <Navbar fixed="top" expand="lg" className="bg-body-tertiary main-nav d-flex align-items" variant="underline">
           <Navbar.Brand href="#home">
-            <img src={logo} className='nav-brand'/>
+            <img src={logo} alt="uqrl logo" className='nav-brand'/>
           </Navbar.Brand>
-          <Nav className="nav-links">
-            <Nav.Link className="nav-tabs">About</Nav.Link>
-            <Nav.Link className="nav-tabs">Events</Nav.Link>
-            <Nav.Link className="nav-tabs">Newsletters</Nav.Link>
-            <Nav.Link className="nav-tabs">Resources</Nav.Link>
-            <Nav.Link className="nav-tabs">Join</Nav.Link>
-          </Nav>
+          {/* <Navbar.Toggle aria-controls="responsive-navbar-nav" /> */}
+            <Nav className="nav-links me-auto">
+              <Nav.Link className="nav-tabs">About</Nav.Link>
+              <Nav.Link className="nav-tabs">Events</Nav.Link>
+              <Nav.Link className="nav-tabs">News</Nav.Link>
+              <Nav.Link className="nav-tabs">Resources</Nav.Link>
+              <Nav.Link className="nav-tabs">Join</Nav.Link>
+            </Nav>  
 
       </Navbar>
     
   );
 }
 
-function BasicExample() {
-  return (
-    <Nav
-      activeKey="/home"
-      onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}
-    >
-      <Nav.Item>
-        <Nav.Link href="/home">Active</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link eventKey="link-1">Link</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link eventKey="link-2">Link</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link eventKey="disabled" disabled>
-          Disabled
-        </Nav.Link>
-      </Nav.Item>
-    </Nav>
-  );
-}
 
 export default App;
