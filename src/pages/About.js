4883b64c64a979a6d905d37da50f58c5f0ea8c54
harri37 <110083492+harri37@users.noreply.github.com>
2024-01-14 12:54:40 +1000

    reorganised file structure

---
+++ src/pages/About.js
@@ -0,0 +1,60 @@
+import React from "react";
+
+function About() {
+  return (
+    <div className="page">
+      <h1 className="page-title">About UQ Reality Labs</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vitae
+        cursus felis. Quisque facilisis augue eget neque scelerisque, in rhoncus
+        eros consectetur. Vivamus eu dolor iaculis, suscipit nisi a, tristique
+        sapien. Etiam pharetra condimentum venenatis. Nulla ultrices turpis
+        luctus, egestas quam rhoncus, dapibus eros. Suspendisse fringilla augue
+        id arcu placerat, ut maximus sapien mollis. Phasellus vel lorem
+        eleifend, varius diam et, varius dolor. Duis ut mauris eget tortor
+        finibus consequat ut ac arcu. Duis sit amet nisi id est molestie aliquet
+        vitae vitae nunc. Etiam tellus est, hendrerit a eros non, condimentum
+        vulputate quam. Donec consectetur mauris velit, sed molestie augue
+        euismod ut. Suspendisse facilisis dolor eget massa lobortis efficitur.
+        Mauris eu molestie tortor. Integer quis sagittis est. Sed lacinia dui
+        sed gravida feugiat. Aliquam eu neque risus. Fusce vitae ipsum congue,
+        ullamcorper nisi a, iaculis massa. Etiam tempus nulla at fermentum
+        gravida. Nam molestie eros ut urna tristique, id interdum ipsum
+        vestibulum. Fusce pellentesque leo non nisi tristique, pretium pharetra
+        purus congue. Nam eu varius libero, eu tincidunt lorem. Aenean eget leo
+        nibh. Pellentesque congue est maximus justo placerat maximus.
+        Pellentesque elit dui, sollicitudin ac elit in, venenatis suscipit
+        tortor. Aenean posuere, ante a sagittis aliquam, tellus ex vehicula
+        ligula, eget ullamcorper nunc lectus sit amet leo. Vivamus sit amet
+        cursus dui. Vivamus at sem justo. Vivamus vulputate cursus bibendum. Sed
+        mollis ligula ipsum, id accumsan nulla pulvinar vel. Nullam rhoncus quis
+        purus id bibendum. Pellentesque vulputate a velit nec scelerisque.
+        Mauris metus magna, hendrerit efficitur porta quis, elementum egestas
+        augue. Etiam eu risus id sem molestie rhoncus. Morbi ex nunc, aliquet at
+        lorem eget, aliquet eleifend orci. Suspendisse a lorem id lectus
+        ullamcorper feugiat a a dolor. Nullam vulputate commodo convallis.
+        Vivamus ut ultrices orci. Phasellus ac blandit sem. Pellentesque turpis
+        odio, placerat ac turpis sed, tempor auctor lectus. In venenatis tortor
+        massa, vitae euismod arcu finibus sit amet. Donec eget venenatis urna.
+        Nullam erat libero, euismod ut nisl eget, pharetra molestie erat. Cras
+        tellus leo, suscipit eget dapibus sit amet, suscipit mollis libero.
+        Curabitur viverra lectus neque, sit amet blandit erat blandit eget.
+        Praesent ut suscipit nisi, id lobortis purus. In consequat ipsum ligula,
+        id imperdiet ante mattis at. Etiam semper nulla eros. Ut venenatis lorem
+        ut elit sodales, sit amet malesuada magna dapibus. Proin dapibus
+        sagittis ipsum, mollis accumsan est commodo eget. Pellentesque semper
+        enim convallis tortor vestibulum, sed eleifend libero feugiat. Etiam a
+        interdum magna, eu rhoncus enim. Vivamus tortor ipsum, dictum vitae
+        iaculis consectetur, scelerisque non mauris. Suspendisse commodo varius
+        nisl, vitae gravida leo tristique sed. Sed vel diam dapibus, efficitur
+        massa sit amet, faucibus quam. Fusce ligula velit, lobortis at
+        ullamcorper nec, fringilla sit amet metus. Aliquam efficitur aliquet
+        dolor, vel mattis tellus tincidunt eu. In non ornare ipsum, eget
+        consectetur turpis. Vestibulum nec imperdiet nisi. Curabitur consequat
+        tincidunt pellentesque.
+      </p>
+    </div>
+  );
+}
+
+export default About;
